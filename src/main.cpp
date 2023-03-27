7ebf2e43ac6922f17f9a581347a7689134c4ba8a
jozefmery <jozefdmery@gmail.com>
2023-03-28 01:16:21 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,31 @@
 #include <devs/lib.hpp>
 #include <iostream>
 
-auto delta_external(const double& s, const double&, const double& input) { return s + input; }
+double delta_external(const double& s, const double&, const double& input) { return s + input; }
 
-auto delta_internal(const double& s) { return s * 2; }
+double delta_internal(const double& s) { return s * 2; }
 
-auto out(const double&) { return -1; }
+double out(const double& state) { return state * 3; }
 
-auto ta(const double& s) { return s; }
+double ta(const double& s) { return s; }
 
 auto create_atomic() {
     using namespace Devs::Model;
-    return Atomic<double, int, double, double>{0.5, delta_external, delta_internal, out, ta};
+    return Atomic<double, double, double, double>{0.5, delta_external, delta_internal, out, ta};
 }
 
 auto create_compound() {
     using namespace Devs::Model;
-    return Compound<double>{{{"component", create_atomic()}}, {}};
+    return Compound<double>{{{"component", create_atomic()}},
+                            {{"component", {{"test model", {}}}}, {"test model", {{"component", {}}}}}};
 }
 
 int main() {
-    auto simulator = Devs::Simulator{"test model", create_compound(), 0.0, 5.0};
+    auto simulator = Devs::Simulator<double>{"test model", create_compound(), 0.0, 5.0};
+    simulator.schedule_model_input(2, 0.5);
+    // simulator.add_model_output_listener([](const std::string& name, const double& time, const Devs::Dynamic& value) {
+    //     std::cout << "Output from: " << name << " at " << time << ", value: " << double{value} << "\n";
+    // });
     simulator.run();
     return 0;
 }
